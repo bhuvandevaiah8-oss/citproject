88c75832a023c8539cbce89e91854cade7efc17b
bhuvandevaiah8-oss <bhuvandevaiah8@gmail.com>
2026-03-26 12:31:19 +0530

    insert

--- Nestedif.java
+++ Nestedif.java
@@ -1,14 +1,2 @@
-public class Nestedif {
-    public static void main(String[] args) {
-        int age=65;
-        if(age>=18){
-            if(age>=65){
-                System.out.println("Elligible to avail government fund");
 
-            }
-        }   else
-                System.out.println("Not eligible to vote");
-
-}
-}
 
